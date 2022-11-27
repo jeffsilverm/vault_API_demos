a877ae463da8c704539970a358a9cf9686d3c8c1
Jeff Silverman <jeffsilverm@gmail.com>
2022-11-27 03:03:24 -0800

    Added test_ansible_mfa.py and found lots of problems

---
+++ ansible/vault2.py
@@ -0,0 +1,42 @@
+# /usr/bin/exec python3
+#
+#
+
+from ansible_mfa import AnsibleMFA
+import random
+
+KEYS = 5
+THRESHOLD = 5
+
+
+def main():
+    # This program implements a complete implementation of the ansible MFA system
+
+    session = AnsibleMFA()
+
+    session.was_started_already = session.is_daemon_running()
+    if not session.was_started_already :
+        session.start_daemon()
+
+    if not session.is_initialized():
+        # root token, keys go in session
+        session.initialize(keys=KEYS, threshold=THRESHOLD)
+
+    if session.is_sealed():
+        session.unseal()
+
+    key = random.randint(0, 1000)
+    value = random.randbytes(8)
+    session.add_secret(key=key, value=value)
+
+    decoded_secret = session.get_secret(key=key)
+    assert value == decoded_secret, f"the value was {value} but the decoded_secret was {decoded_secret}"
+
+    session.seal()
+
+    if not session.was_started_already:
+        session.stop_daemon()
+
+
+if "__main__" == __name__:
+    main()
